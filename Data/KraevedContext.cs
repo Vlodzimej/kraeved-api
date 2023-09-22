e8709cd6186cb912863fda6d5632500aaa60c2c3
Владимир <amel-07@mail.ru>
2023-09-22 13:07:09 +0300

    Added GeoObject

---
+++ Data/KraevedContext.cs
@@ -0,0 +1,14 @@
+﻿using KraevedAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KraevedAPI.Data
+{
+    public class KraevedContext: DbContext
+    {
+        public KraevedContext(DbContextOptions<KraevedContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GeoObject> RpgCharacters => Set<GeoObject>();
+    }
+}
---
+++ Models/GeoObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace KraevedAPI.Models
+{
+    /// <summary>
+    /// Географический объект
+    /// </summary>
+    public class GeoObject
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Широта
+        /// </summary>
+        /// 
+        public double Latitude { get; set; }
+        /// <summary>
+        /// Долгота
+        /// </summary>
+        public double Longitude { get; set; }
+    }
+}
