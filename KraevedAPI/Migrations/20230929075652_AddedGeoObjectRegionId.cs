4fa1f391f66e2bf86743a1df27829c7fecae4b45
Владимир <amel-07@mail.ru>
2023-09-29 10:59:04 +0300

    Added GeoObject RegionId migration

---
+++ KraevedAPI/Migrations/20230929075652_AddedGeoObjectRegionId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KraevedAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedGeoObjectRegionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RegionId",
+                table: "GeoObjects",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegionId",
+                table: "GeoObjects");
+        }
+    }
+}
