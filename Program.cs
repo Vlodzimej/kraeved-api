750b978f49f8810d514fdda3f60abd6ad6c1387f
Vladimir Amelkin <v.amelkin@scloud.ru>
2026-04-05 11:16:40 +0300

    fix: increase request body size limit to 100MB for image uploads

--- Program.cs
+++ Program.cs
@@ -36,6 +36,12 @@ builder.Services.AddControllers(options =>
 {
     options.Filters.Add<HttpResponseExceptionFilter>();
 });
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Features.FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 104857600; // 100MB
+    options.ValueLengthLimit = 104857600;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
