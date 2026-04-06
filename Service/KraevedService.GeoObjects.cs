cac721e020790d22c681dca5884f99bd8c5c8d09
Vladimir Amelkin <v.amelkin@scloud.ru>
2026-04-06 21:33:39 +0300

    fix: added category to get geobject request

--- Service/KraevedService.GeoObjects.cs
+++ Service/KraevedService.GeoObjects.cs
@@ -14,7 +14,7 @@ namespace KraevedAPI.Service
         {
             var filter = new GeoObjectFilter() { Id = id };
             var result = _unitOfWork.GeoObjectsRepository
-                .Get(x => filter.Id == null || x.Id == filter.Id, includeProperties: "Type,PersonGeoObjects.Person,Images")
+                .Get(x => filter.Id == null || x.Id == filter.Id, includeProperties: "Type,Type.Category,PersonGeoObjects.Person,Images")
                 ?? throw new Exception(ServiceConstants.Exception.UnknownError);
 
             var geoObject = result.FirstOrDefault();
