6d16f725f2363de70d1ec476a24277213f44824c
Vladimir Amelkin <v.amelkin@scloud.ru>
2026-04-06 20:29:26 +0300

    feat: add OKN category with types and uppercase category names migration

---
+++ Migrations/20260406172531_AddOKNCategoryAndTypes.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kraeved.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOKNCategoryAndTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO ""GeoObjectCategories"" (""Id"", ""Name"", ""Title"") VALUES
+                (6, 'okn', 'Объекты культурного наследия')
+                ON CONFLICT (""Id"") DO NOTHING;
+
+                INSERT INTO ""GeoObjectTypes"" (""Id"", ""Name"", ""Title"", ""CategoryId"") VALUES
+                (601, 'MONUMENT', 'Памятник', 6),
+                (602, 'ENSEMBLE', 'Ансамбль', 6),
+                (603, 'NOTABLE_PLACE', 'Достопримечательное место', 6)
+                ON CONFLICT (""Id"") DO NOTHING;
+
+                ALTER SEQUENCE ""GeoObjectCategories_Id_seq"" RESTART WITH 7;
+                ALTER SEQUENCE ""GeoObjectType_Id_seq"" RESTART WITH 604;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM ""GeoObjectTypes"" WHERE ""Id"" IN (601, 602, 603);
+                DELETE FROM ""GeoObjectCategories"" WHERE ""Id"" = 6;
+
+                ALTER SEQUENCE ""GeoObjectCategories_Id_seq"" RESTART WITH 6;
+                ALTER SEQUENCE ""GeoObjectType_Id_seq"" RESTART WITH 506;
+            ");
+        }
+    }
+}
---
+++ Migrations/20260406172802_UppercaseGeoObjectCategoryName.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kraeved.Migrations
+{
+    /// <inheritdoc />
+    public partial class UppercaseGeoObjectCategoryName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""GeoObjectCategories"" SET ""Name"" = UPPER(""Name"");
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""GeoObjectCategories"" SET ""Name"" = LOWER(""Name"");
+            ");
+        }
+    }
+}
